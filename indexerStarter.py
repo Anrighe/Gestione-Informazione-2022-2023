54176cae5b59a964cbeb62d5e6cb6aefcdd76180
Anrighe <assoonpc@gmail.com>
2023-01-29 16:12:37 +0100

    minor change

--- indexerStarter.py
+++ indexerStarter.py
@@ -3,7 +3,7 @@ from indexer import Indexer
 
 # Script Arguments:
 # The first argument is the dataset file (.csv)
-# The second argument is the folder in which the Indexing will take place
+# The second argument is the folder in which the indexing will take place
 if __name__ == "__main__":
     indexer = Indexer(sys.argv[1], sys.argv[2])
     indexer.indexGenerator()
